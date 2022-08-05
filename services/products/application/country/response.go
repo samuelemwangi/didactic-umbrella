dc6a7b96bb5fc8c06084716b938b098f5b581e2f
samuelemwangi <mwangi.s.irungu@gmail.com>
2022-08-06 00:26:32 +0300

    improve product set up - response object

--- services/products/application/country/response.go
+++ services/products/application/country/response.go
@@ -2,17 +2,30 @@ package country
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/samuelemwangi/jumia-mds-test/services/products/domain"
 )
 
-type CountryResponseDTO struct {
+type ProductDetail struct {
 	ID          string `json:"id"`
 	CountryName string `json:"countrName"`
 }
 
-func (c *CountryResponseDTO) toResponseDTO(entity *domain.Country) {
-	c.ID = fmt.Sprint(entity.ID)
-	c.CountryName = entity.Name
+type CountryResponseDTO struct {
+	Status  int            `json:"responseStatus"`
+	Message string         `json:"responseMessage"`
+	Item    *ProductDetail `json:"itemDetails"`
+}
 
+func (c *CountryResponseDTO) toResponseDTO(entity *domain.Country) {
+
+	productDetail := &ProductDetail{
+		ID:          fmt.Sprint(entity.ID),
+		CountryName: entity.Name,
+	}
+
+	c.Status = http.StatusOK
+	c.Message = "request successful"
+	c.Item = productDetail
 }
