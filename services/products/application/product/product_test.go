cee06793db8667c0fd23ef2dfe8704a5180a2ef0
samuelemwangi <mwangi.s.irungu@gmail.com>
2022-08-09 11:09:29 +0300

    add unit tests for application/product -  request.go & response.go

--- services/products/application/product/product_test.go
+++ services/products/application/product/product_test.go
@@ -1,7 +1,9 @@
 package product
 
 import (
+	"net/http"
 	"testing"
+	"time"
 
 	"github.com/samuelemwangi/jumia-mds-test/services/products/domain"
 )
@@ -60,14 +62,107 @@ func TestValidProductRequestDTOValidateRequest(t *testing.T) {
 func TestProductResponseToDTO(t *testing.T) {
 	t.Run("Test toProductResponseDTO() method", func(t *testing.T) {
 
-		product := &domain.Product{
-			SKU: "123H",
-		}
-		response := ProductResponseDTO{}
-		response.toProductResponseDTO(product)
+		sku := "123H"
+		productName := "Sample Name"
 
-		if response.Item.SKU != "123H" {
-			t.Errorf("Expected SKU to be 123, got %s", response.Item.SKU)
+		product := &domain.Product{
+			SKU:  sku,
+			Name: productName,
+		}
+		product.ID = 1
+		product.CreatedAt = time.Now()
+		product.UpdatedAt = time.Now()
+
+		productResponse := ProductResponseDTO{}
+		productResponse.toProductResponseDTO(product)
+
+		if productResponse.Status != http.StatusOK {
+			t.Errorf("Expected status to be 200, got %d", productResponse.Status)
+		}
+
+		if productResponse.Message != "request successful" {
+			t.Errorf("Expected message to be request successful, got %s", productResponse.Message)
+		}
+
+		if productResponse.Item.ID != product.ID {
+			t.Errorf("Expected product id to be %d, got %d", product.ID, productResponse.Item.ID)
+		}
+
+		if productResponse.Item.SKU != product.SKU {
+			t.Errorf("Expected product sku to be %s, got %s", product.SKU, productResponse.Item.SKU)
+		}
+
+		if productResponse.Item.Name != product.Name {
+			t.Errorf("Expected product name to be %s, got %s", product.Name, productResponse.Item.Name)
+		}
+
+		if productResponse.Item.CreatedAt == (time.Time{}).Format("2006-01-02 15:04:05") {
+			t.Errorf("Expected created at to be set, got %s", productResponse.Item.CreatedAt)
+		}
+
+		if productResponse.Item.UpdatedAt == (time.Time{}).Format("2006-01-02 15:04:05") {
+			t.Errorf("Expected updated at to be set, got %s", productResponse.Item.UpdatedAt)
+		}
+	},
+	)
+}
+
+func TestProductsResponseToDTO(t *testing.T) {
+	t.Run("Test toProductsResponseDTO() method", func(t *testing.T) {
+
+		sku := "123H"
+		productName := "Sample Name"
+
+		product1 := &domain.Product{
+			SKU:  sku + "1",
+			Name: productName + "1",
+		}
+		product1.ID = 1
+
+		product2 := &domain.Product{
+			SKU:  sku + "2",
+			Name: productName + "2",
+		}
+		product2.ID = 2
+
+		products := []*domain.Product{product1, product2}
+		productsResponse := ProductsResponseDTO{}
+		productsResponse.toProductsResponseDTO(products)
+
+		if productsResponse.Status != http.StatusOK {
+			t.Errorf("Expected status to be 200, got %d", productsResponse.Status)
+		}
+
+		if productsResponse.Message != "request successful" {
+			t.Errorf("Expected message to be request successful, got %s", productsResponse.Message)
+		}
+
+		if len(productsResponse.Items) != 2 {
+			t.Errorf("Expected 2 products, got %d", len(productsResponse.Items))
+		}
+
+		if productsResponse.Items[0].ID != product1.ID {
+			t.Errorf("Expected product id to be %d, got %d", product1.ID, productsResponse.Items[0].ID)
+		}
+
+		if productsResponse.Items[0].SKU != product1.SKU {
+			t.Errorf("Expected product sku to be %s, got %s", product1.SKU, productsResponse.Items[0].SKU)
+		}
+
+		if productsResponse.Items[0].Name != product1.Name {
+			t.Errorf("Expected product name to be %s, got %s", product1.Name, productsResponse.Items[0].Name)
+		}
+
+		if productsResponse.Items[1].ID != product2.ID {
+			t.Errorf("Expected product id to be %d, got %d", product2.ID, productsResponse.Items[1].ID)
+		}
+
+		if productsResponse.Items[1].SKU != product2.SKU {
+			t.Errorf("Expected product sku to be %s, got %s", product2.SKU, productsResponse.Items[1].SKU)
+		}
+
+		if productsResponse.Items[1].Name != product2.Name {
+			t.Errorf("Expected product name to be %s, got %s", product2.Name, productsResponse.Items[1].Name)
 		}
 	},
 	)
